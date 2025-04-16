2bcdbd5a1f4581f05b2270d2d473bf31cbd2911a
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-16 17:37:44 -0300

    feat: create page home missing little adjusts and your logic

---
+++ src/app/components/formulary-login/formulary-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulary-login',
+  imports: [],
+  templateUrl: './formulary-login.component.html',
+  styleUrl: './formulary-login.component.css'
+})
+export class FormularyLoginComponent {
+
+}
