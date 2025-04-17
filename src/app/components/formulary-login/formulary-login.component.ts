0c98c42358a2dc4eee71f4dc843775cbf03f8315
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-17 20:23:28 -0300

    feat: implement checkbox on login formulary

--- src/app/components/formulary-login/formulary-login.component.ts
+++ src/app/components/formulary-login/formulary-login.component.ts
@@ -2,17 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { InputComponent } from '../input/input.component';
 import { InputPasswordComponent } from '../input-password/input-password.component';
+import { CheckboxComponent } from "../checkbox/checkbox.component";
 
 @Component({
   selector: 'app-formulary-login',
   standalone: true,
-  imports: [CommonModule, InputComponent, InputPasswordComponent],
+  imports: [CommonModule, InputComponent, InputPasswordComponent, CheckboxComponent],
   templateUrl: './formulary-login.component.html',
   styleUrls: ['./formulary-login.component.css']
 })
 export class FormularyLoginComponent {
   email = '';
   password = '';
+  checkbox = false;
 
   onSubmit() {
     console.log('Form Data:', { email: this.email, password: this.password });
