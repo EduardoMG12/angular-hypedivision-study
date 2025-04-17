20334385815581448e141ba422565f43cedf31db
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-17 01:56:14 -0300

    feat(login-form): componentize login form

--- src/app/components/formulary-login/formulary-login.component.ts
+++ src/app/components/formulary-login/formulary-login.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { InputComponent } from '../input/input.component';
+import { InputPasswordComponent } from '../input-password/input-password.component';
 
 @Component({
   selector: 'app-formulary-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, InputComponent, InputPasswordComponent],
   templateUrl: './formulary-login.component.html',
-  styleUrl: './formulary-login.component.css'
+  styleUrls: ['./formulary-login.component.css']
 })
 export class FormularyLoginComponent {
+  email = '';
+  password = '';
 
+  onSubmit() {
+    console.log('Form Data:', { email: this.email, password: this.password });
+  }
 }
---
+++ src/app/types/types.ts
@@ -0,0 +1,18 @@
+export type HTMLInputTypeAttribute =
+	| "number"
+	| "text"
+	| "search"
+	| "button"
+	| "time"
+	| "image"
+	| "checkbox"
+	| "color"
+	| "date"
+	| "datetime-local"
+	| "email"
+	| "file"
+	| "hidden"
+	| "month"
+	| "password"
+	| "radio"
+	| "range";
