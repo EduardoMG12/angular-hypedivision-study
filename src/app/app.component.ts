a731316ce277219562fbb4d5bafa60994e32bcff
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-17 20:48:51 -0300

    feat: create routes and beggin development page not-found

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from './pages/login/login.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
