76aeb581a70cfbde82ac4ba175e2a1c602f89c8a
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-17 20:48:51 -0300

    feat: create routes and beggin development page not-found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     // In future i want implement lazyLoading
+    // { path: '', loadChildren: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
+    // { path: 'register', loadChildren: () => import('./pages/register/register.component').then(m => m.RegisterComponent) },
+    { path: '', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', component: NotFoundComponent }
+];
