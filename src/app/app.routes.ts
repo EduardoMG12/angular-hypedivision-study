a928111966afecbc1a6d3fdf81c98f09fb358ee0
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-19 15:20:08 -0300

    feat: create first section to page home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DevelopmentComponent } from './pages/development/development.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
      // In future i want implement lazyLoading
     // { path: '', loadChildren: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
     // { path: 'register', loadChildren: () => import('./pages/register/register.component').then(m => m.RegisterComponent) },
-    { path: '', component: LoginComponent },
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'development', component: DevelopmentComponent },
     { path: '**', component: NotFoundComponent }
