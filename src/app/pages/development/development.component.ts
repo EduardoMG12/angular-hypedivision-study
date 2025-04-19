5281813cdeccc4bd7a68542d6e4029457f0a6520
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-19 12:39:10 -0300

    feat:create component challenge-card and implement real data to page development

--- src/app/pages/development/development.component.ts
+++ src/app/pages/development/development.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { CardComponent } from '../../components/card/card.component';
+import { CommonModule } from '@angular/common';
+import { ChallengeCardComponent } from '../../components/challenge-card/challenge-card.component';
 
 @Component({
   selector: 'app-development',
-  imports: [HeaderComponent, CardComponent],
+  imports: [CommonModule,HeaderComponent, CardComponent, ChallengeCardComponent],
   templateUrl: './development.component.html',
   styleUrl: './development.component.css'
 })
