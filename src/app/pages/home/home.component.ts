e8efa9ea916144d4d6192d362b780593c251bce6
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-22 22:08:34 -0300

    feat: create page home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CardStackComponent } from '../../components/card-stack/card-stack.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CardStackComponent],
+  imports: [HeaderComponent, CardStackComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
