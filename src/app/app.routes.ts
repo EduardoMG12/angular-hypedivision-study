c93c7263f3843d11d0ac1ff4c7067a703166ee5b
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-24 01:33:36 -0300

    refactor: change name home to welcome

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DevelopmentComponent } from './pages/development/development.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
      // In future i want implement lazyLoading
     // { path: '', loadChildren: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
     // { path: 'register', loadChildren: () => import('./pages/register/register.component').then(m => m.RegisterComponent) },
-    { path: '', component: HomeComponent },
+    { path: '', component: WelcomeComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'development', component: DevelopmentComponent },
