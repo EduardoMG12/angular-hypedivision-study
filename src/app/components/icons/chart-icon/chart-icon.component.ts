37029a825bed4d5eb93c1490ba336ad48871eec6
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-25 00:44:32 -0300

    feat: add some icons to side-bard and your directive for reutilize code

---
+++ src/app/components/icons/chart-icon/chart-icon.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { SvgStrokeDirective } from '../../../directive/svg-stroke.directive';
+
+@Component({
+  selector: 'app-chart-icon',
+  standalone:true,
+  imports: [CommonModule, SvgStrokeDirective],
+  templateUrl: './chart-icon.component.html',
+  styleUrl: './chart-icon.component.css'
+})
+export class ChartIconComponent {
+  @Input() stroke = '#FAFAFA';
+}
---
+++ src/app/components/icons/settings-icon/settings-icon.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { SvgStrokeDirective } from '../../../directive/svg-stroke.directive';
+
+@Component({
+  selector: 'app-settings-icon',
+  standalone:true,
+  imports: [CommonModule, SvgStrokeDirective],
+  templateUrl: './settings-icon.component.html',
+  styleUrl: './settings-icon.component.css'
+})
+export class SettingsIconComponent {
+  @Input() stroke = '#FAFAFA';
+}
