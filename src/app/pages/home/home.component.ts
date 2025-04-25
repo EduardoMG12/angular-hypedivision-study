0ecc4ebe8f0fefca303608b00c20179aa8e73972
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-25 19:03:48 -0300

    feat:create html structure to homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
+import { CardStackComponent } from './../../components/card-stack/card-stack.component';
 import { Component } from "@angular/core";
 import { SideBarComponent } from "../../components/side-bar/side-bar.component";
 
 @Component({
 	selector: "app-home",
 	standalone: true,
-	imports: [SideBarComponent],
+	imports: [SideBarComponent,CardStackComponent],
 	templateUrl: "./home.component.html",
 	styleUrl: "./home.component.css",
 })
