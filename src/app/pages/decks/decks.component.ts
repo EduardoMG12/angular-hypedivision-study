976c1d8aff7a40842010e05366eb2a2bfafb82bd
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-26 15:20:24 -0300

    feat: create static first section decks page

--- src/app/pages/decks/decks.component.ts
+++ src/app/pages/decks/decks.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from '../../components/side-bar/side-bar.component';
+import { PlusIconComponent } from '../../components/icons/plus-icon/plus-icon.component';
 
 @Component({
   selector: 'app-decks',
-  imports: [SideBarComponent],
+  imports: [SideBarComponent, PlusIconComponent],
   templateUrl: './decks.component.html',
   styleUrl: './decks.component.css'
 })
