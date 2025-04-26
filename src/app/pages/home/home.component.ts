731383fb4330b192bdd6f40e08c7ee8d7bbc119b
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-26 14:47:09 -0300

    refactor: finally static style home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,16 +2,14 @@ import { CommonModule } from '@angular/common';
 import { CardStackComponent } from './../../components/card-stack/card-stack.component';
 import { Component } from "@angular/core";
 import { SideBarComponent } from "../../components/side-bar/side-bar.component";
-import { OpenBookIconComponent } from '../../components/icons/open-book-icon/open-book-icon.component';
-import { AlbumIconComponent } from '../../components/icons/album-icon/album-icon.component';
-import { BookMarkIconComponent } from '../../components/icons/book-mark-icon/book-mark-icon.component';
 import { CardWihtIconComponent } from "../../components/card-with-icon/card-with-icon.component";
 import { RecentDeckCardComponent } from '../../components/recent-deck-card/recent-deck-card.component';
+import { PlusIconComponent } from '../../components/icons/plus-icon/plus-icon.component';
 
 @Component({
 	selector: "app-home",
 	standalone: true,
-	imports: [CommonModule, SideBarComponent, CardStackComponent, CardWihtIconComponent, RecentDeckCardComponent],
+	imports: [CommonModule, SideBarComponent, CardStackComponent, CardWihtIconComponent, RecentDeckCardComponent, PlusIconComponent],
 	templateUrl: "./home.component.html",
 	styleUrl: "./home.component.css",
 })
