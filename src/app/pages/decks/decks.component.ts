472833f7faf0d210f9d934af0514e9ddf1d7f28d
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-26 15:59:48 -0300

    feat: finalize html and css static page decks

--- src/app/pages/decks/decks.component.ts
+++ src/app/pages/decks/decks.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from '../../components/side-bar/side-bar.component';
 import { PlusIconComponent } from '../../components/icons/plus-icon/plus-icon.component';
+import { LupeIconComponent } from '../../components/icons/lupe-icon/lupe-icon.component';
+import { DeckCardComponent } from '../../components/deck-card/deck-card.component';
 
 @Component({
   selector: 'app-decks',
-  imports: [SideBarComponent, PlusIconComponent],
+  imports: [SideBarComponent, PlusIconComponent,LupeIconComponent, DeckCardComponent],
   templateUrl: './decks.component.html',
   styleUrl: './decks.component.css'
 })
