aa7d2b33fbec2dfc26ac2e0c8c8da9723dae559d
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-25 22:34:16 -0300

    refactor: componentize cardWithIcon and implement to home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,11 +5,12 @@ import { SideBarComponent } from "../../components/side-bar/side-bar.component";
 import { OpenBookIconComponent } from '../../components/icons/open-book-icon/open-book-icon.component';
 import { AlbumIconComponent } from '../../components/icons/album-icon/album-icon.component';
 import { BookMarkIconComponent } from '../../components/icons/book-mark-icon/book-mark-icon.component';
+import { CardWihtIconComponent } from "../../components/card-with-icon/card-with-icon.component";
 
 @Component({
 	selector: "app-home",
 	standalone: true,
-	imports: [CommonModule, SideBarComponent,CardStackComponent, OpenBookIconComponent, AlbumIconComponent, BookMarkIconComponent],
+	imports: [CommonModule, SideBarComponent, CardStackComponent, CardWihtIconComponent],
 	templateUrl: "./home.component.html",
 	styleUrl: "./home.component.css",
 })
