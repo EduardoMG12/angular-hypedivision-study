d5af5b1358edebb1ac521972adb41185a2944a6c
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-26 15:59:48 -0300

    feat: finalize html and css static page decks

---
+++ src/app/components/deck-card/deck-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deck-card',
+  imports: [],
+  templateUrl: './deck-card.component.html',
+  styleUrl: './deck-card.component.css'
+})
+export class DeckCardComponent {
+
+}
