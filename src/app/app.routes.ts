4b4d8a64930ec9091a1c76232349113c1b4618a5
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-28 15:12:36 -0300

    feat: implement all routes with SSR

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,64 @@ import type { Routes } from "@angular/router";
 
 export const routes: Routes = [
 	{ path: "", redirectTo: "/welcome", pathMatch: "full" },
-  { path: "welcome", loadComponent: () => import("./pages/welcome/welcome.component").then(m => m.WelcomeComponent) },
-	{ path: "home", loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
-	{ path: "login", loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
-	{ path: "register", loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent) },
-	{ path: "development", loadComponent: () => import('./pages/development/development.component').then(m => m.DevelopmentComponent) },
-	{ path: "decks", loadComponent: () => import('./pages/decks/decks.component').then(m => m.DecksComponent) },
-	{ path: "create-flashcard", loadComponent: () => import('./pages/create-flashcard/create-flashcard.component').then(m => m.CreateFlashcardComponent) },
-	{ path: "statistics", loadComponent: () => import('./pages/statistics/statistics.component').then(m => m.StatisticsComponent), data: { ssr: false } },
+	{
+		path: "welcome",
+		loadComponent: () =>
+			import("./pages/welcome/welcome.component").then(
+				(m) => m.WelcomeComponent,
+			),
+	},
+	{
+		path: "home",
+		loadComponent: () =>
+			import("./pages/home/home.component").then((m) => m.HomeComponent),
+	},
+	{
+		path: "login",
+		loadComponent: () =>
+			import("./pages/login/login.component").then((m) => m.LoginComponent),
+	},
+	{
+		path: "register",
+		loadComponent: () =>
+			import("./pages/register/register.component").then(
+				(m) => m.RegisterComponent,
+			),
+	},
+	{
+		path: "development",
+		loadComponent: () =>
+			import("./pages/development/development.component").then(
+				(m) => m.DevelopmentComponent,
+			),
+	},
+	{
+		path: "decks",
+		loadComponent: () =>
+			import("./pages/decks/decks.component").then((m) => m.DecksComponent),
+		data: { ssr: false },
+	},
+	{
+		path: "create-flashcard",
+		loadComponent: () =>
+			import("./pages/create-flashcard/create-flashcard.component").then(
+				(m) => m.CreateFlashcardComponent,
+			),
+	},
+	{
+		path: "statistics",
+		loadComponent: () =>
+			import("./pages/statistics/statistics.component").then(
+				(m) => m.StatisticsComponent,
+			),
+		data: { ssr: false },
+	},
 	// { path: 'settings', component: SettingsComponent },
-	{ path: "**", loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent) },
+	{
+		path: "**",
+		loadComponent: () =>
+			import("./pages/not-found/not-found.component").then(
+				(m) => m.NotFoundComponent,
+			),
+	},
 ];
