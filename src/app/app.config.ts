14374241a63bb1743fa4f50a9472a88eb61cc772
Eduardo Mello <102185386+EduardoMG12@users.noreply.github.com>
2025-04-28 18:35:09 -0300

    Revert "feat(frontend): add initial pages, components, and mock data with SSR…" (#2)
    This reverts commit 74bcbd50c37f3c88c2da21a6f02067a79e251545.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,9 @@
-// biome-ignore lint/style/useImportType: <explanation>
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-import { routes } from "./app.routes";
-import {
-	provideClientHydration,
-	withEventReplay,
-} from "@angular/platform-browser";
-import { provideCharts, withDefaultRegisterables } from "ng2-charts";
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-	providers: [
-    provideCharts(withDefaultRegisterables()),
-		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideRouter(routes),
-		provideClientHydration(withEventReplay()),
-    provideAnimations()
-	],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
 };
--- src/app/directive/svg-stroke.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SvgStrokeDirective } from './svg-stroke.directive';
-
-describe('SvgStrokeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SvgStrokeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
